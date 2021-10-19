b1c429ff5a90549f769c09e75ea58e45eb2b0b09
erdem doganay <erdemdoganayiletisim@gmail.com>
2021-10-19 19:02:26 +0300

    18 Ekim 2021

---
+++ src/app.js
@@ -0,0 +1,13 @@
+let konutKredileri = [
+    "Konut Kredisi",
+    "İhtiyaç Kredisi",
+    "Emlak Konut Kredisi",
+    "Taşıt Kredisi",
+    "Tüketici Kredisi"
+];
+
+console.log("<ul>")
+for(let i = 0; i<konutKredileri.length; i++) {
+console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
